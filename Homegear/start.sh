2e9edb7324d893eb11f5a07d42c7508dd61bff10
Nicolas COLLET <nicolas.collet@ubiflow.net>
2018-05-24 13:41:19 +0200

    debian version

--- Homegear/start.sh
+++ Homegear/start.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-# Inspired by https://github.com/Homegear/Homegear-Docker/blob/master/rpi-stable/start.sh
+# Inspired by https://github.com/Homegear/Homegear-Docker/blob/master/stable/start.sh
 
 mkdir -p /config/homegear /share/homegear/lib /share/homegear/log
 chown homegear:homegear /config/homegear /share/homegear/lib /share/homegear/log
